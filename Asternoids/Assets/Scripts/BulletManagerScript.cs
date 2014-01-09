3485b251f27c399c91fb1a9fb51a8ef6be6cb71a
kevroy314 <kevin.horecka@gmail.com>
2014-01-09 13:30:17 -0600

    Bullet Hit Animation
    x1. basic animations and sound
    x2. low enemyhp
    x3. higher rate of fire
    x4. more enemies
    x5. bigger bullets
    6. mussle flash
    x7. faster bullets
    8. less accuracy
    x9. impact effects
    10. hit animation
    11. enemy knockback
    12. permanence
    x13. camera lerp
    x14. camera position
    15. screenshake
    16. player knockback
    17. sleep
    18 gun delay
    19. gun kick
    x20. strafe
    21. more performance
    22. more bass
    23. super machine gun
    24. random explosions
    25. faster enemies
    26. more enemies
    27. even higher rate of fire and camera kick
    28. bigger explosions
    29. even more permanence
    30. meaning
    
    Soon TODO:
    -Mussle Flash animation on bullets when launched
    -Enemy bullets?
    -Ship acceleration animation
    -Reduce accuracy on player weapon (enemy weapon?)
    -Add enemy HP
    -Add hit animation for enemies

--- Asternoids/Assets/Scripts/BulletManagerScript.cs
+++ Asternoids/Assets/Scripts/BulletManagerScript.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class BulletManagerScript : MonoBehaviour {
 	//Distance from origin for memory cleanup
 	public float destroyDistance = 200f; //Distance from origin at which point a bullet disappears
+	private Animator anim;
 
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
 	//Update is called once per frame
 	void Update () {
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(rigidbody2D.velocity.y, rigidbody2D.velocity.x) * Mathf.Rad2Deg - 90f));
 		//Check boundry conditions and destroy if exceeded
-		if(transform.position.magnitude > destroyDistance)
+		if(transform.position.magnitude > destroyDistance || anim.GetCurrentAnimatorStateInfo (0).IsTag ("DestroyObject"))
+		{
+			Debug.Log ("Destroy");
 			DestroyObject (gameObject);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
-		DestroyObject (gameObject);
+		tag = "Untagged";
+		anim.SetBool("BulletHit",true);
 	}
 }
